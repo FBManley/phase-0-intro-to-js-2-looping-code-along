5f25d0a2f153ebdfb36ff8f9d284b03bddfe20be
Jacob Manley <jbmanley45@gmail.com>
2022-02-28 14:35:03 -0800

    commit tree 915dd7d5106be901f17e91171883a8b1f0153e53

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+
+
+function writeCards(recipients, event) {
+    let array = []
+    
+    for (let i = 0; i < recipients.length; i++){
+    array.push(`Thank you, ${recipients[i]}, for the wonderful ${event} gift!`);
+    
+    }
+    return array; 
+}
+
+
